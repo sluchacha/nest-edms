101b77174d1d0d72b93bbcc3a19b134f34d9c834
sluchacha <luchacha.s@gmail.com>
2022-01-18 02:23:54 +0300

    Began unit testing users

---
+++ src/users/test/stubs/user.stub.ts
@@ -0,0 +1,16 @@
+import { User } from '../../entities';
+import { Role } from '../../enums';
+import { Types } from 'mongoose';
+
+export const userStub = (): User => {
+  return {
+    _id: new Types.ObjectId().toHexString(),
+    firstName: 'Stephen',
+    lastName: 'Luchacha',
+    email: 'luchacha.s@gmail.com',
+    password: '12345678',
+    role: Role.ADMIN,
+    isActive: true,
+    lastAccessedOn: new Date(),
+  };
+};
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,79 +2,258 @@ import { NotFoundException } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Connection, Model } from 'mongoose';
+import { createMockModel, MockModel } from '../database/test/support';
+import { CreateUserDto } from './dto';
 import { User, UserDocument } from './entities';
+import { userStub } from './test/stubs';
+import { UserModel } from './test/support/user.model';
 import { UsersService } from './users.service';
 
-// A Mock Model type which represents an object that consists of some of the
-// properties that the Model type contains as well. However all these values
-// are of type jest.Mock - a mock  function provided by jest
-type MockModel<T = any> = Partial<Record<keyof Model<T>, jest.Mock>>;
-
-// A generic function that returns the newly defined MockModel class
-// when the type argument is not provided it falls back to any
-const createMockModel = <T = any>(): MockModel<T> => ({
-  findOne: jest.fn(() => {
-    return {
-      exec: jest.fn(() => {
-        id: '1';
-      }),
-    };
-  }),
-  create: jest.fn(),
-});
-
 describe('UsersService', () => {
-  let service: UsersService;
-  let userModel: MockModel;
+  describe.skip('Find Operations', () => {
+    let service: UsersService;
+    let userModel: MockModel<User>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        { provide: Connection, useValue: {} },
-        {
-          provide: getModelToken(User.name),
-          useValue: createMockModel<UserDocument>(),
-        },
-      ],
-    }).compile();
+    beforeAll(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          UsersService,
+          { provide: Connection, useValue: {} },
+          {
+            provide: getModelToken(User.name),
+            useValue: createMockModel<User>(),
+          },
+        ],
+      }).compile();
 
-    service = module.get<UsersService>(UsersService);
-    userModel = module.get<MockModel<UserDocument>>(getModelToken(User.name));
-  });
+      service = module.get<UsersService>(UsersService);
+      userModel = module.get<MockModel<User>>(getModelToken(User.name));
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
 
-  describe('findOne', () => {
-    describe('when user with ID exists', () => {
-      it('should return the user object', async () => {
-        const userId = '1';
-        const expectedUser = { id: '1' };
+    describe('findAll', () => {
+      let users: User[];
 
-        //userModel.findOne.mockReturnValue(expectedUser);
-        /* const spy = jest
-          .spyOn(userModel, 'findOne')
-          .mockResolvedValue(expectedUser); */
-        const user = await service.findOne(userId);
-        // console.log({ user });
-        expect(user).toEqual(expectedUser);
+      beforeEach(async () => {
+        users = [userStub()];
+      });
+
+      it('should call the user model', async () => {
+        jest.spyOn(userModel, 'find').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(users),
+          };
+        });
+        expect.assertions(1);
+        await service.findAll();
+        expect(userModel.find).toHaveBeenCalled();
+      });
+
+      it('should return a list of users', async () => {
+        jest.spyOn(userModel, 'find').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(users),
+          };
+        });
+        expect.assertions(2);
+        const result = await service.findAll();
+        expect(result.length).toBe(1);
+        expect(
+          result.some((u) => u.firstName === users[0].firstName),
+        ).toBeTruthy();
       });
     });
 
-    describe('otherwise, when user does not exist', () => {
-      it.skip('should throw the "NotFoundException"', async () => {
-        const userId = '1';
-        userModel.findOne.mockReturnValue(undefined);
+    describe('findOneByEmail', () => {
+      let user: User;
 
+      beforeEach(async () => {
+        user = userStub();
+      });
+
+      it('should call the user model by specifying the user email', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+        expect.assertions(1);
+        await service.findOneByEmail(user.email);
+        expect(userModel.findOne).toHaveBeenCalledWith({ email: user.email });
+      });
+
+      it('should return the user object when user with email exists', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+        expect.assertions(1);
+        const result = await service.findOneByEmail(user.email);
+        expect(result).toMatchObject(user);
+      });
+
+      it('should return undefined when user with specified email does not exist', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(null),
+          };
+        });
+        expect.assertions(1);
+        const result = await service.findOneByEmail(user.email);
+        expect(result).toBe(undefined);
+      });
+    });
+
+    describe('findOneById', () => {
+      let user: User;
+
+      beforeEach(async () => {
+        user = userStub();
+      });
+
+      it('should call the user model by specifying the user id', async () => {
+        jest.spyOn(userModel, 'findById').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+        expect.assertions(1);
+        await service.findOneById(user._id);
+        expect(userModel.findById).toHaveBeenCalledWith(user._id);
+      });
+
+      it('should return the user object when user with ID exists', async () => {
+        jest.spyOn(userModel, 'findById').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+        expect.assertions(1);
+        const result = await service.findOneById(user._id);
+        expect(result).toMatchObject(user);
+      });
+
+      it('should return undefined when user with specified id does not exist', async () => {
+        jest.spyOn(userModel, 'findById').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(null),
+          };
+        });
+        expect.assertions(1);
+        const result = await service.findOneById(user._id);
+        expect(result).toBe(undefined);
+      });
+    });
+
+    describe('findOne', () => {
+      let user: User;
+
+      beforeEach(async () => {
+        user = userStub();
+      });
+
+      it('should call the user model by specifying the user id', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+        expect.assertions(1);
+        await service.findOne(user._id);
+        expect(userModel.findOne).toHaveBeenCalledWith({ _id: user._id });
+      });
+
+      it('should return the user object when user with ID exists', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(user),
+          };
+        });
+
+        const result = await service.findOne(user._id);
+        expect(result).toMatchObject(user);
+      });
+
+      it('should throw the "NotFoundException" when user with specified id does not exist', async () => {
+        jest.spyOn(userModel, 'findOne').mockImplementation(() => {
+          return {
+            exec: jest.fn().mockResolvedValue(null),
+          };
+        });
+        expect.assertions(2);
         try {
-          await service.findOne(userId);
+          await service.findOne(user._id);
         } catch (err) {
           expect(err).toBeInstanceOf(NotFoundException);
-          expect(err.message).toEqual(`User #${userId} not found`);
+          expect(err.message).toMatch(/not found/i);
         }
       });
     });
   });
+
+  describe('create operations', () => {
+    let service: UsersService;
+    let userModel: UserModel;
+
+    beforeEach(async () => {
+      const moduleRef = await Test.createTestingModule({
+        providers: [
+          UsersService,
+          {
+            provide: getModelToken(User.name),
+            useValue: UserModel,
+          },
+        ],
+      }).compile();
+
+      service = moduleRef.get<UsersService>(UsersService);
+      userModel = moduleRef.get<UserModel>(getModelToken(User.name));
+    });
+
+    describe('create', () => {
+      describe('when create is called', () => {
+        let user: User;
+        let saveSpy: jest.SpyInstance;
+        let constructorSpy: jest.SpyInstance;
+        let dto: CreateUserDto;
+        let result: any;
+
+        beforeEach(async () => {
+          saveSpy = jest.spyOn(UserModel.prototype, 'create');
+          constructorSpy = jest.spyOn(UserModel.prototype, 'constructorSpy');
+          user = userStub();
+          dto = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: user.password,
+            password_confirm: user.password,
+            role: user.role,
+          };
+
+          result = await service.create(dto);
+        });
+
+        test('then it should call the userModel', () => {
+          expect.assertions(3);
+          expect(constructorSpy).toHaveBeenCalledWith(dto);
+          expect(saveSpy).toHaveBeenCalled();
+          expect(service.create(dto))
+            .resolves.toEqual(user)
+            .catch((err) => {
+              console.debug(err);
+            });
+        });
+
+        // test('then it should return a user', () => {
+        //   expect(result).toEqual(user);
+        // });
+      });
+    });
+  });
+  // expect({name: 'Peter Parker',}).toHaveProperty('name', expect.stringMatching(/peter/i))
 });
