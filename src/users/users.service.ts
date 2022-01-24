58a4a7b9982ebc2d3d1266a8d09ee99af78f23ec
sluchacha <luchacha.s@gmail.com>
2022-01-24 20:07:56 +0300

    Completed unit test on users service and updated comments

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,21 +20,20 @@ export class UsersService {
 
   /**
    * @summary Creates a user
-   * @param {CreateUserDto} createUserDto user details, the email
-   * must be unique
-   * @returns {Promise<User>} The user with created details or throws an error
-   * @throws {BadRequestException} If the user is already registered
+   * @param {CreateUserDto} createUserDto user details, the email must be unique
+   * @returns {Promise<User>} The user with created details
+   * @throws {BadRequestException} Throws an error if the user is already registered
    */
   async create(createUserDto: CreateUserDto): Promise<User> {
     const { email } = createUserDto;
 
     // Check if user exists using email
-    // const exists = await this.userModel.findOne({ email }).exec();
-    // if (exists) throw new BadRequestException('The user is already registered');
+    const exists = await this.userModel.findOne({ email }).exec();
+    if (exists) throw new BadRequestException('The user is already registered');
 
-    const user = new this.userModel(createUserDto);
+    // const user = new this.userModel(createUserDto);
     // return user.save();
-    return this.userModel.create(user);
+    return this.userModel.create(createUserDto);
   }
 
   /**
@@ -48,31 +47,44 @@ export class UsersService {
   }
 
   /**
-   * @summary Returns a user by their unique email or undefined
+   * @summary Returns a user by their unique email or undefined.
+   * Mainly used for authentication
    * @param {string} email email of user, not case sensitive
    * @returns {(Promise<UserDocument | undefined>)}
    * @memberof UsersService
    */
   async findOneByEmail(email: string): Promise<UserDocument | undefined> {
     const user = await this.userModel.findOne({ email }).exec();
+
     if (user) return user;
+
+    return undefined;
+  }
+
+  /**
+   * @summary Returns a user by their unique id.
+   * Mainly used for authorization
+   * @param {string} id user id
+   * @returns {(Promise<UserDocument | undefined>)}
+   * @memberof UsersService
+   */
+  async findOneById(id: string): Promise<UserDocument | undefined> {
+    const user = await this.userModel.findById(id).exec();
+
+    if (user) return user;
+
     return undefined;
   }
 
   /**
    * @summary Returns a user by their unique id
    * @param {string} id user id
-   * @returns {(Promise<User>)}
+   * @returns {(Promise<User>)} User found
    * @memberof UsersService
    */
-  async findOneById(id: string): Promise<UserDocument | undefined> {
-    const user = await this.userModel.findById(id).exec();
-    if (user) return user;
-    return undefined;
-  }
-
   async findOne(id: string): Promise<User> {
     const user = await this.userModel.findOne({ _id: id }).exec();
+
     if (!user) {
       throw new NotFoundException(`User #${id} not found`);
     }
@@ -80,6 +92,12 @@ export class UsersService {
     return user;
   }
 
+  /**
+   * @summary Updates a user with specified id
+   * @param {string} id user id
+   * @param {UpdateUserDto} updateUserDto  user details to be updated
+   * @returns {(Promise<User>)} The updated user
+   */
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
     // Prepare object to update nested object fields separately
     dot.keepArray = true;
@@ -90,13 +108,13 @@ export class UsersService {
       .exec();
 
     if (!user)
-      throw new NotFoundException(`The user with the given id was not found`);
+      throw new BadRequestException(`The user with the given id was not found`);
 
     return user;
   }
 
   /**
-   * summary Delete a user
+   * @summary Delete a user with specified id
    * @param {string} id
    * @returns {(Promise<User>)} The deleted user
    * @todo Check for related records before deleting
